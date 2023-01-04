c46282de49c6ea61a8e429d939ee7e85f2af5d33
a_lot_of_milk <munzimelo@ewhain.net>
2023-01-04 13:39:26 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 40 ms, Memory: 30644 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
@@ -0,0 +1,9 @@
+import sys
+
+N = int(sys.stdin.readline())
+for i in range(1,N+1):
+    for j in range(N,i,-1):
+        print(" ", end="")
+    for k in range(0,i):
+        print("*", end="")
+    print("")
