26817084bdf7333cfb89b2bdc88130f8cd44cbaa
a_lot_of_milk <munzimelo@ewhain.net>
2023-01-16 14:32:00 +0900

    [Bronze III] Title: 점수계산, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2506. 점수계산/점수계산.py
@@ -0,0 +1,13 @@
+import sys
+N = int(sys.stdin.readline())
+results = list(map(int,sys.stdin.readline().split())); cnt = 0; score = 0
+for i in range(N):
+    if results[i] == 1:
+        if cnt == 0:
+            cnt += 1
+            score += cnt
+        elif cnt > 0:
+            cnt += 1
+            score += cnt
+    else: cnt = 0
+print(score) 
