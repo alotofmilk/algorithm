c82e46de2fec07ab936ad9fb67223fb5e237d82e
a_lot_of_milk <munzimelo@ewhain.net>
2023-01-17 15:41:40 +0900

    [Bronze V] Title: A+B - 5, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -1,14 +1,7 @@
 import sys
-
-A = int(1)
-B = int(1)
-sum = []
-while not(A == 0 and B == 0):
-    A, B = map(int, (sys.stdin.readline().split()))
-    sum.append(A+B)
-
-for i in range(0,len(sum)):
-    if sum[i] != 0:
-        print(sum[i])
-    else:
-        break
+answer = []
+while True:
+    A, B = map(int,sys.stdin.readline().split())
+    if A == 0 and B == 0: break
+    else: answer.append(A+B)
+print(*answer, sep="\n")
