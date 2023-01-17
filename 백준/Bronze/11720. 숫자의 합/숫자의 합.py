37601bd79852337c1c1ccb8e89e5fa3b36397826
a_lot_of_milk <munzimelo@ewhain.net>
2023-01-17 16:19:08 +0900

    [Bronze IV] Title: 숫자의 합, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.py
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.py
@@ -1,5 +1,5 @@
 import sys
 N = int(sys.stdin.readline())
-numbers = sys.stdin.readline(); sum = 0
-for i in range(N): sum += int(numbers[i])
-print(sum)
+numbers = sys.stdin.readline().strip(); result = 0
+for i in range(len(numbers)): result += int(numbers[i])
+print(result)
