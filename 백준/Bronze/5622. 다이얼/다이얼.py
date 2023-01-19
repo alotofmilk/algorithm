0ff69fdcad7b39096156a10c864b0ab5a5c8d46c
a_lot_of_milk <munzimelo@ewhain.net>
2023-01-19 14:43:58 +0900

    [Bronze II] Title: 다이얼, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,11 +1,8 @@
 import sys
-code = sys.stdin.readline()
-
-telephone = ["ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"]; get_number = 0; time = 0
-for i in range(len(code)):
-    for j in range(len(telephone)):
-        if code[i] in telephone[j]:
-            get_number = j + 2
-            time += get_number + 1
-            get_number = 0
-print(time)
+dial = {"ABC":3,"DEF":4,"GHI":5,"JKL":6,"MNO":7,"PQRS":8,"TUV":9,"WXYZ":10}
+word = sys.stdin.readline().strip(); count = 0
+dial_keys_list = list(dial.keys())
+for i in range(len(word)):
+    for j in range(len(dial_keys_list)):
+        if word[i] in dial_keys_list[j]: count += dial[dial_keys_list[j]]
+print(count)
