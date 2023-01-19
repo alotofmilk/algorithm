722066baf95d146bb8b6a05dd175937693b8cf01
a_lot_of_milk <munzimelo@ewhain.net>
2023-01-19 13:51:47 +0900

    [Bronze III] Title: 홀수, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2576. 홀수/홀수.py
+++ 백준/Bronze/2576. 홀수/홀수.py
@@ -1,6 +1,7 @@
+import sys
 odd_numbers = []
 for i in range(7):
-    number = int(input())
+    number = int(sys.stdin.readline().strip())
     if number % 2 == 1: odd_numbers.append(number)
 if len(odd_numbers) == 0: print(-1)
 else: print(sum(odd_numbers), min(odd_numbers), sep="\n")
