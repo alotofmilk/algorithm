4c38ae412bb2b4e0c82438e5bbcb24d7482784d8
a_lot_of_milk <munzimelo@ewhain.net>
2023-01-26 11:00:54 +0900

    [Silver IV] Title: 제로, Time: 88 ms, Memory: 32032 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,7 +1,9 @@
 import sys
-K = int(sys.stdin.readline()); result = []
+K = int(sys.stdin.readline()); stack = []; result = 0
+numbers = [int(sys.stdin.readline().strip()) for i in range(K)]
 for i in range(K):
-    money = int(sys.stdin.readline().strip())
-    if money != 0: result.append(money)
-    else: result.pop()
-print(sum(result))
+    if numbers[i] != 0:
+        stack.append(numbers[i])
+    else: stack.pop()
+for i in range(len(stack)): result += stack[i]
+print(result)
