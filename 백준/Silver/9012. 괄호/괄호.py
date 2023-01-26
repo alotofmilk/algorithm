c7e60e3098663dcadf8eab7fd8dcc4fc47b1376c
a_lot_of_milk <munzimelo@ewhain.net>
2023-01-26 14:11:57 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,10 +1,17 @@
 import sys
-T = int(sys.stdin.readline())
+T = int(sys.stdin.readline()); sentence = []; stack = []
 for i in range(T):
-    sentence = sys.stdin.readline().strip()
-    while "()" or "*" in sentence:
-        if "()" in sentence: sentence = sentence.replace("()", "*")
-        elif "*" in sentence: sentence = sentence.replace("*", "")
-        elif "()" or "*" not in sentence: break
-    if len(sentence) == 0: print("YES")
-    else: print("NO")
+    sentence.append(sys.stdin.readline().strip())
+    for j in range(len(sentence[i])):
+        if sentence[i][j] == "(": stack.append(sentence[i][j])
+        elif sentence[i][j] == ")":
+            if len(stack) > 0: stack.pop()
+            else:
+                stack.append(sentence[i][j])
+                break
+    if len(stack) == 0:
+        print("YES")
+        stack.clear()
+    else:
+        print("NO")
+        stack.clear()
