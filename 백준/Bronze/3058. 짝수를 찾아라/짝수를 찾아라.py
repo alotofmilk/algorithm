c8913e4073d50ac09f307905624c73b55c4b21de
a_lot_of_milk <munzimelo@ewhain.net>
2023-02-15 00:00:34 +0900

    [Bronze III] Title: 짝수를 찾아라, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/3058. 짝수를 찾아라/짝수를 찾아라.py
+++ 백준/Bronze/3058. 짝수를 찾아라/짝수를 찾아라.py
@@ -1,9 +1,9 @@
-x = int(input())
-
-for i in range(x):
-    even_num = []
-    a = list(map(int, input().split()))
-    for i in a:
-        if i % 2 == 0:
-            even_num.append(i)
-    print(sum(even_num), min(even_num))
+import sys
+T = int(sys.stdin.readline().strip()); even = []
+for _ in range(T):
+    numbers = list(map(int,sys.stdin.readline().split()))
+    for i in range(len(numbers)):
+        if numbers[i] % 2 == 0:
+            even.append(numbers[i])
+    print(sum(even),min(even))
+    even.clear()
