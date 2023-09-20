b40ea71fc42d4a8cd21a0b3bef6dee121e7c43c5
a_lot_of_milk <munzimelo@ewhain.net>
2023-09-20 23:30:45 +0900

    [Bronze V] Title: A+B - 7, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -1,10 +1,7 @@
 import sys
-
-T = int(sys.stdin.readline())
-sum = []
+T = int(sys.stdin.readline()); data = []
 for i in range(0,T):
     A, B = map(int,sys.stdin.readline().split())
-    sum.append(A+B)
-
+    data.append(A+B)
 for i in range(0,T):
-    print("Case #{}: {}".format(i+1,sum[i]))
+    print("Case #{}: {}".format(i+1, data[i]))
