1c0b1c4dc6d3b8b6234d042e41da4fdebb20d851
a_lot_of_milk <munzimelo@ewhain.net>
2023-09-20 23:32:41 +0900

    [Bronze V] Title: A+B - 8, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -1,14 +1,7 @@
 import sys
-
-T = int(sys.stdin.readline())
-num1 = []
-num2 = []
-sum = []
+T = int(sys.stdin.readline()); data = []
 for i in range(0,T):
-    A, B = map(int, sys.stdin.readline().split())
-    num1.append(A)
-    num2.append(B)
-    sum.append(A + B)
-
+    A, B = map(int,sys.stdin.readline().split())
+    data.append([A,B,A+B])
 for i in range(0,T):
-    print("Case #{}: {} + {} = {}".format(i+1,num1[i],num2[i],sum[i]))
+    print("Case #{}: {} + {} = {}".format(i+1, data[i][0], data[i][1], data[i][2]))
