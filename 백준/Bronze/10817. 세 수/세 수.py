a01fd446cc87bcf2afdfeddced91a6df010851ab
a_lot_of_milk <munzimelo@ewhain.net>
2023-09-26 22:10:07 +0900

    [Bronze III] Title: 세 수, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.py
+++ 백준/Bronze/10817. 세 수/세 수.py
@@ -1,6 +1,4 @@
 import sys
-import heapq
-A, B, C = map(int,sys.stdin.readline().split()); min_heap = []
-heapq.heappush(min_heap,A); heapq.heappush(min_heap,B); heapq.heappush(min_heap,C)
-heapq.heappop(min_heap)
-print(min_heap[0])
+A,B,C = map(int,sys.stdin.readline().split()); list = [A,B,C]
+list.remove(max(list))
+print(max(list))
