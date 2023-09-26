e79b98d2062940150b10660a68280d625b0e332d
a_lot_of_milk <munzimelo@ewhain.net>
2023-09-26 20:07:53 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -1,7 +1,7 @@
 import sys
 score = int(sys.stdin.readline())
 if score >= 90: print("A")
-elif 80 <= score <= 89: print("B")
-elif 70 <= score <= 79: print("C")
-elif 60 <= score <= 69: print("D")
+elif 80 <= score and score < 90: print("B")
+elif 70 <= score and score < 80: print("C")
+elif 60 <= score and score < 70: print("D")
 else: print("F")
