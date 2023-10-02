204e4229ca40481b2ce8db6080d801be93413e8b
a_lot_of_milk <munzimelo@ewhain.net>
2023-10-02 17:05:13 +0900

    [Bronze V] Title: 윤년, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,13 +1,7 @@
-year = input()
-year = int(year)
-
+import sys
+year = int(sys.stdin.readline())
 if year % 4 == 0:
-    if year % 100 != 0:
-        print("1")
-    elif year % 100 == 0:
-        if year % 400 == 0:
-            print("1")
-        elif year % 400 != 0:
-            print("0")
-elif year % 4 != 0:
-    print("0")
+    if year % 100 != 0: print(1)
+    elif year % 400 == 0: print(1)
+    else: print(0)
+else:print(0)
