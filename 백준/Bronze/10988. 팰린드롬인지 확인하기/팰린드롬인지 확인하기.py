fd32933f7d5545acefa7c744b546b85f1de8142a
a_lot_of_milk <munzimelo@ewhain.net>
2024-01-11 21:30:46 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,9 +1,14 @@
 import sys
-word = sys.stdin.readline().strip()
-reverse_word = word[::-1]; cnt = ""
-for i in range(len(word)):
-    if word[i] == reverse_word[i]: cnt += "*"
-    else:
-        print(0)
-        break
-if len(cnt) == len(word): print(1)
+sentence = sys.stdin.readline().strip(); flag = 0
+if len(sentence) % 2 == 1:
+    for i in range(0, (len(sentence) + 1) // 2):
+        if sentence[i] == sentence[len(sentence)-1-i]: pass
+        else: flag = 1
+    if flag == 0: print(1)
+    else: print(0)
+else:
+    for i in range(len(sentence) // 2):
+        if sentence[i] == sentence[len(sentence)-1-i]: pass
+        else: flag = 1
+    if flag == 0: print(1)
+    else: print(0)
